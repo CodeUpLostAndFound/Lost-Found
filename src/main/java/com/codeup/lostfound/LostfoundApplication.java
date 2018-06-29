a033126eb76b5a37e085118bfc454e0e99dfcee0
lancecrowder <thelancecrowder@gmail.com>
2018-06-29 13:39:20 -0500

    index.html created

--- src/main/java/com/codeup/lostfound/LostfoundApplication.java
+++ src/main/java/com/codeup/lostfound/LostfoundApplication.java
@@ -3,10 +3,12 @@ package com.codeup.lostfound;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class LostfoundApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LostfoundApplication.class, args);
+		run(LostfoundApplication.class, args);
 	}
 }
