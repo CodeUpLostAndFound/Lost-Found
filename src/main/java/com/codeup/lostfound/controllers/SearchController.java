4be3f10c6696211f4e2582315860b11543563a32
danielconner <metalman1342@gmail.com>
2018-07-02 11:44:28 -0500

    wired up get mapping

--- src/main/java/com/codeup/lostfound/controllers/SearchController.java
+++ src/main/java/com/codeup/lostfound/controllers/SearchController.java
@@ -1,4 +1,22 @@
 package com.codeup.lostfound.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class SearchController {
+
+    @GetMapping("search")
+    public String locSearch(Model model) {
+
+        return "search/locSearch";
+    }
+
+    @PostMapping("search")
+    public String catSearch() {
+
+        return "search/catSearch";
+    }
 }
