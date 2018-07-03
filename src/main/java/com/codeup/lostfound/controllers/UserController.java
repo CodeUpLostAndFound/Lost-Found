112fcab1199f74c119ea9ed285c8fdcd94bf1fc9
lancecrowder <thelancecrowder@gmail.com>
2018-07-03 15:56:52 -0500

    EOD Push

--- src/main/java/com/codeup/lostfound/controllers/UserController.java
+++ src/main/java/com/codeup/lostfound/controllers/UserController.java
@@ -1,18 +1,45 @@
 package com.codeup.lostfound.controllers;
 
+import com.codeup.lostfound.models.User;
+import com.codeup.lostfound.repositories.UserRepository;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
+    private UserRepository users;
+//    private PasswordEncoder passwordEncoder;
+
+
+    public UserController(UserRepository users/*PasswordEncoder passwordEncoder*/){
+        this.users = users;
+//        this.passwordEncoder = passwordEncoder;
+    }
+
     @GetMapping("/")
     public String landing(Model model) {
 
+        return "redirect:users/login";
+    }
+
+    @GetMapping("users/login")
+    public String login(Model model) {
+
         return "users/login";
+    }
+
+    @PostMapping("users/login")
+    public String loggingIn(@ModelAttribute User user) {
+//        String hash = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(hash);
+        users.save(user);
+        return "redirect:items";
     }
 
     @GetMapping("users/register")
